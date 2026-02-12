fa2cda9400af42aeb1a003ce919ddfef59ae9a3f
itsukakotori55 <ikotori55@gmail.com>
2026-02-12 15:23:53 +0700

    feat: add new data education

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -108,6 +108,23 @@ export const biodataCurr: ICurrVitae = {
               deserunt mollit anim id est laborum.
           `,
     },
+    {
+      degree: 'Diploma',
+      major: 'Civil Engineering',
+      university: 'POLBAN',
+      gpa: '3.5/4.0',
+      gpaStatus: 'Cumlaude',
+      graduated: moment('2020-09-10').format('YYYY-MM-DD'),
+      graduatedStatus: true,
+      majorDesc: `
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum.
+          `,
+    },
   ],
   certification: [
     {
